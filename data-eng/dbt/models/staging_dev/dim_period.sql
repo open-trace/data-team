f73d645f4fdf84285f38e7da5835ef61434f2e32
corneilleopentrace <corneille.opentrace@gmail.com>
2026-05-27 15:04:37 +0200

    DPL-007: build star schema dims and facts in staging_dev
    Point staging at raw_dev sources that exist in BigQuery, fix dim_period for BigQuery DATE(), and add transformation documentation.
    
    # Conflicts:
    #	data-eng/docs/SOIL_DASHBOARD_TRANSFORMATION.md
    #	dbt/models/silver/stg_silver_star_metrics.sql

--- data-eng/dbt/models/staging_dev/dim_period.sql
+++ data-eng/dbt/models/staging_dev/dim_period.sql
@@ -3,6 +3,6 @@
 select distinct
     cast(period_year as int64) as period_key,
     cast(period_year as int64) as period_year,
-    make_date(cast(period_year as int64), 1, 1) as start_date
+    date(cast(period_year as int64), 1, 1) as start_date
 from {{ ref('stg_silver_star_metrics') }}
 where period_year is not null
